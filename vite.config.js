786f5096c041d10edfd783c375d0c3281c64ead0
Umindu Dinal <umindudinal0318@gmail.com>
2026-06-12 21:48:43 +0530

    fix base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Umindu_Dinal_Portfolio/',
+  base: '/',
 })
